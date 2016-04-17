5e4d47e31df77ea48202a302c7fb900b5d701d4b
Dan Shapiro <dan.shapiro1210@gmail.com>
2016-04-16 17:40:19 -0700

    Add round-to-card relationship

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,6 @@ activerecord = Deck.create(name: "ActiveRecord")
 http = Deck.create(name: "HTTP")
 cs_concepts = Deck.create(name: "CS Concepts")
 agile = Deck.create(name: "Agile")
-<<<<<<< HEAD
-=======
 
-sinatra = Deck.find(1)
-sinatra.cards.create(question: "Is this a question?", answer: true)
->>>>>>> d2d72c7a2e1987cca9e83112296e026dc2f584cb
+# sinatra = Deck.find(1)
+# sinatra.cards.create(question: "Is this a question?", answer: true)
