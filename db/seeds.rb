79c90f640cd5fe8707b780bc32767c0204191ff7
Dan Shapiro <dan.shapiro1210@gmail.com>
2016-04-16 17:33:34 -0700

    Add seeds

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+sinatra = Deck.create(name: "Sinatra")
+javascript = Deck.create(name: "JavaScript")
+activerecord = Deck.create(name: "ActiveRecord")
+http = Deck.create(name: "HTTP")
+cs_concepts = Deck.create(name: "CS Concepts")
+agile = Deck.create(name: "Agile")
+
+# sinatra = Deck.find(1)
+# sinatra.cards.create(question: "Is this a question?", answer: true)
